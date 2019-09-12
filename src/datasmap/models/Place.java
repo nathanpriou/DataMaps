0e6e3fe7accdd8002b5db0b8b35b8a87611fc35f
nathanpriou <priounathan@gmail.com>
2019-09-12 10:51:14 +0200

    first commit

--- src/datasmap/models/Place.java
+++ src/datasmap/models/Place.java
@@ -1,10 +1,25 @@
 package datasmap.models;
 
+import java.util.List;
+
 public class Place {
-	String address;
-	double latitude;
-	double longitude;
+	protected String address;
+	protected double latitude;
+	protected double longitude;
+	private Place parent;
+	private List<Site> sites;
 	
+	public Place(String address, double latitude, double longitude) {
+		this(address,latitude,longitude,null); //repere via nom methode + types de parametres + null en ref au parent 
+	}
+
+	public Place(String address, double latitude, double longitude, Place parent) {
+		this.address = address;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.parent = parent;
+	}
+
 	public String getAddress() {
 		//retourner l'adresse complète du lieu
 		return address;
